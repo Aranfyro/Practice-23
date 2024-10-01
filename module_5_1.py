156b89a13a7249dd2917aafbcd847be3b04ff722
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-10-01 14:56:54 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,19 @@
+# Домашняя работа по уроку "Атрибуты и методы объекта."
+# Задача "Developer - не только разработчик":
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = int(number_of_floors)
+
+    def go_to (self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range (1, new_floor + 1):
+                    print (i)
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
